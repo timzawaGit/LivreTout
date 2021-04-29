c17b81040ae5e39cd4e140e396a9386e81de6d9d
Vanak <ven2kaze@gmail.com>
2021-04-29 20:15:04 +0200

    added junit for UserDao

---
+++ src/test/java/fr/doranco/livretout/dao/UserDaoTest.java
@@ -0,0 +1,19 @@
+package fr.doranco.livretout.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserDaoTest {
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserByEmail() {
+		fail("Not yet implemented");
+	}
+
+}
